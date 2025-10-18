f09a7d0334557984c6e5b61e17c6b1f0c50476ab
David Varela <d.varela1@uniandes.edu.co>
2025-10-18 17:42:35 -0500

    commit

--- src/logica/Administrador.java
+++ src/logica/Administrador.java
@@ -1,5 +1,16 @@
 package logica;
 
+import java.util.ArrayList;
+
 public class Administrador {
+	private String usuario;
+	private String contrasena;
+	private ArrayList<Solicitud> solicitudes;
+
+	
+	public boolean login(String usuario, String contrasena) {
+		return (usuario.equals(this.usuario) && contrasena.equals(this.contrasena));
+		
+	}
 
 }
--- src/logica/BoletasMaster.java
+++ src/logica/BoletasMaster.java
@@ -1,19 +1,45 @@
 package logica;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class BoletasMaster {
 	
 	
 	private boolean esAdministrador;
 	private boolean esOrganizador;
 	private boolean esCliente;
+	private ArrayList<Venue> venues;
+	private HashMap<String, Tiquete> tiquetes;
+	private HashMap<String, Organizador> organizadores;
+	private HashMap<String, Cliente> clientes;
+	private HashMap<String, Usuario> usuarios;
+	private Administrador administrador; 
+	
+	
+	
+
+	
+	
 	
 	public BoletasMaster() {
 		esAdministrador = false;
 		esOrganizador = false;
 		esCliente = false;
-		hola 
+	    this.venues = new ArrayList<Venue>();
+	    this.tiquetes = new HashMap<String, Tiquete>();
+	    this.organizadores = new HashMap<String, Organizador>();
+	    this.clientes = new HashMap<String, Cliente>();
+	    this.usuarios = new HashMap<String, Usuario>();
+		
+		 
 	
 
+	}
+	
+	public void solicitarReembolsoCalamidad() {
+		
+		
 	}
 
 }
