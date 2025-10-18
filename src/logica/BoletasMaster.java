933e393f1ff6a174a46fd67515e81aa0ecaed93f
David Varela <d.varela1@uniandes.edu.co>
2025-10-18 16:01:20 -0500

    .

--- src/logica/BoletasMaster.java
+++ src/logica/BoletasMaster.java
@@ -1,5 +1,19 @@
 package logica;
 
 public class BoletasMaster {
+	
+	
+	private boolean esAdministrador;
+	private boolean esOrganizador;
+	private boolean esCliente;
+	
+	public BoletasMaster() {
+		esAdministrador = false;
+		esOrganizador = false;
+		esCliente = false;
+		
+	
+
+	}
 
 }
